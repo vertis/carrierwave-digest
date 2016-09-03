1232ca6dbcd2fd301c3221cfd61fe08134187696
Luke Chadwick <me@vertis.io>
2016-09-03 12:12:17 +1000

    Basic functionality and tests

--- lib/carrierwave/digest.rb
+++ lib/carrierwave/digest.rb
@@ -1,7 +1,30 @@
 require "carrierwave/digest/version"
+require 'digest/sha1'
 
 module Carrierwave
   module Digest
-    # Your code goes here...
+    # Modified from https://github.com/carrierwaveuploader/carrierwave/wiki/How-to%3a-Use-file%27s-digest-%28e.g.-MD5,-SHA-1%29-as-file-path
+    def self.included(base)
+      base.extend(ClassMethods)
+    end
+    module ClassMethods
+      def ensure_digest_for(name)
+        define_method("#{name}_digest") do
+          if send("#{name}_changed?") &&
+            send("#{name}").file.present? &&
+            send("#{name}").file.respond_to?(:path) &&
+            File.exists?(send("#{name}").file.path)
+            ::Digest::SHA1.file(send("#{name}").file.path).hexdigest
+          else # Reading image
+            self["#{name}_digest".to_sym]
+          end
+        end
+
+        define_method("update_#{name}_digest") do
+          self.send("#{name}_digest=", send("#{name}_digest")) if send("#{name}_changed?")
+        end
+        before_save "update_#{name}_digest".to_sym
+      end
+    end
   end
 end
--- spec/carrierwave/digest_spec.rb
+++ spec/carrierwave/digest_spec.rb
@@ -1,11 +1,30 @@
 require 'spec_helper'
 
+class DigestTest
+  include Carrierwave::Digest
+  ensure_digest_for :image
+end
+
 describe Carrierwave::Digest do
   it 'has a version number' do
     expect(Carrierwave::Digest::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'should generate a digest for an image that has just been added' do
+    d = DigestTest.new
+    image = double('image')
+    file = File.open('LICENSE.txt')
+    expect(file).to receive(:present?).and_return(true)
+    expect(image).to receive(:file).at_least(:once).and_return(file)
+    expect(d).to receive(:image).at_least(:once).and_return(image)
+    expect(d).to receive(:image_changed?).and_return(true)
+    expect(d.image_digest).to eq("626cfc9a343c4567bf035120b422b84e20735cc8")
+  end
+
+  it 'should return the existing digest stored in the database' do
+    d = DigestTest.new
+    expect(d).to receive(:[]).with(:image_digest).and_return("1234")
+    expect(d).to receive(:image_changed?).and_return(false)
+    expect(d.image_digest).to eq("1234")
   end
 end
